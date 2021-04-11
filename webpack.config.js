d3b4c7f9e8754d93e2d35a5e08a803e76bf70f11
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2021-04-11 15:43:04 -0400

    learning new tech

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ webpack的配置文件
 */
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -20,7 +21,9 @@ module.exports = {
         // 多个loader处理用use
         use: [
           // 创建style标签，将js中的样式资源插入，添加到head中生效
-          "style-loader",
+          // "style-loader",
+          // 这个loader 取代 style-loader 提取js中的css成为单独文件
+          MiniCssExtractPlugin.loader,
           // 将css文件变成commonjs模块加载js中，里面内容是样式字符串
           "css-loader",
         ],
@@ -35,8 +38,10 @@ module.exports = {
         // 下载两个 url-loader file-loader
         loader: 'url-loader',
         options: {
-          limit: 8 * 1024
-
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          // 关闭es6模块化
+          exModules: false,
         }
       },
       {
@@ -46,6 +51,11 @@ module.exports = {
       },
       {
         // 打包其他资源（除了html/js/css以外的资源)
+        exclude: /\.(html|js|css|less|jpg|png|gif)/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+        }
       }
     ],
   },
@@ -59,8 +69,26 @@ module.exports = {
     new HtmlWebpackPlugin({
       // 复制 './src/index.html' 文件,并自动引入打包输出所有的资源
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // 对输出的css文件进行重命名
+      filename: 'css/built.css'
     })
   ],
 
   mode: "development",
+  // 开发服务器 devServer 用来自动化（自动编译，自动打开浏览器，自动刷新浏览器）
+  // 特点： 指挥在内存中编译打包，不会有任何输出
+  // 启动devServer指令为： npx webpack-dev-server
+  // 安装 npm i webpack-dev-server -D
+  devServer: {
+    // 项目构建后路径
+    contentBase: resolve(__dirname, 'build'),
+    // 启动gzip压缩
+    compress: true,
+    // 端口号
+    port: 3000,
+    // 自动打开浏览器
+    open: true
+  }
 };
